0f1f4bf9e01e99083e68d5ccdee3f1247507d3e4
Rojin Abraham <rojinabraham@gmail.com>
2020-06-03 11:54:54 +0530

    updated js

--- script.js
+++ script.js
@@ -9,8 +9,43 @@ async function searchSongs(term) {
   const data = await res.json();
   showData(data);
 }
-function showData() {}
+function showData(data) {
+  let output = "";
+  result.innerHTML = `
+  <ul class="songs">  
+   ${data.data
+     .map(
+       (song) => `<li>
+                  <span><strong>${song.artist.name}</strong>-${song.title}</span>
+                  <button class='btn' data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+                  </li>`
+     )
+     .join("")}
+  </ul>`;
 
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class='btn' onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ""
+      }
+      ${
+        data.next
+          ? `<button class='btn' onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ""
+      }
+      `;
+  } else {
+    more.innerHTML = "";
+  }
+  //console.log(data.next);
+}
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+  showData(data);
+}
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const searchTerm = search.value.trim();
